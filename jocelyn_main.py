a9847ac7d7dc087c9afd7190d7c1b42a6f25e113
Jocelyn Gomez <92837117+JoceG@users.noreply.github.com>
2024-03-13 23:48:03 -0700

    updated execution function

--- jocelyn_main.py
+++ jocelyn_main.py
@@ -1,6 +1,36 @@
 import sys
 
+def execute_command(db_connection, sql_command):
+    '''
+    Executes sql command in connection.
+
+    :return: Bool, optional results
+    '''
+    try:
+        cursor = db_connection.cursor()
+        print("Successfully connected to the database")
+        print("Initialization begin")
+
+        cursor.execute(sql_command)
+        result = cursor.fetchall()
+
+        print(result)
+
+        db_connection.commit()
+        print("Initialization end successfully")
+        return True, result
+    except Exception as e:
+        print(e)
+        return False
+
 def insertStudent(argv): #task 2
+    '''
+    Insert a new student into the related tables.
+
+    argv - UCINetID, email, First, Middle, Last
+    return: bool
+    '''
+
     sql_command = """
         INSERT INTO User (UCINetID, email, First, Middle, Last)
         VALUES (argv[2], argv[3], argv[4], argv[5], argv[6])
@@ -8,19 +38,35 @@ def insertStudent(argv): #task 2
         INSERT INTO Student (UCINetID)
         VALUES (argv[2])
     """
-
-    #return bool??
+    
+    res = execute_command(db_connection, sql_command)
+    print(res[0])
 
 def insertMachine(argv): #task 5
+    '''
+    Insert a new machine.
+
+    argv - MachineID, hostname, IPAddr, status, location
+    return: bool
+    '''
+    
     sql_command = """
         INSERT INTO Machine (MachineID, hostname, IPAddr, status)
         VALUES (argv[2], argv[3], argv[4], argv[5])
 
     """
 
-    #return bool??
+    res = execute_command(db_connection, sql_command)
+    print(res[0])
 
 def listCourse(argv): # task 8
+    '''
+    Given a student ID, list all unique courses the student attended. Ordered by courseId ascending.
+
+    argv - UCINetID
+    return: Table - CourseId,title,quarter
+    '''
+    
     sql_command = """
         SELECT DISTINCT
             c.CourseID, c.*
@@ -39,7 +85,18 @@ def listCourse(argv): # task 8
         
     """
 
+    res = execute_command(db_connection, sql_command)
+    print(res[1])
+
 def activeStudent(argv): # task 11
+    '''
+    Given a machine Id, find all active students that used it more than N times (including N) in a 
+    specific time range (including start and end date). Ordered by netid ascending. N will be at least 1.
+
+    argv - MachineID, N, start, end
+    return: Table - UCINetId,first name,middle name,last name
+    '''
+    
     sql_command = """
         SELECT
             s.UCINetID, s.*
@@ -61,6 +118,9 @@ def activeStudent(argv): # task 11
         ORDER BY
             s.UCINetID ASC 
     """
+
+    res = execute_command(db_connection, sql_command)
+    print(res[1])
 
 if __name__ == "__main__":
     #argv[0] = project.py
